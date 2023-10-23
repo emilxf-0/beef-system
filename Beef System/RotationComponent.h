4e74253b44e07627be9d032b22146ecbd6e92641
Emil Forsén <emilxforsen@gmail.com>
2023-10-23 15:12:18 +0200

    Hotfix rotation component

---
+++ Beef System/RotationComponent.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#include "Components.h"
+#include "SDL.h"
+#include "TextureManager.h"
+
+class RotationComponent : public Component
+{
+
+public:
+
+	SDL_Point rotationCenter;
+	double angle = 0;
+
+	RotationComponent() = default;
+
+	RotationComponent(double angle)
+	{
+		this->angle = angle;
+	}
+
+
+	void init() override
+	{
+	
+	}
+
+	void update() override
+	{
+		//angle += 1;
+	}
+};
