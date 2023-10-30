01163974dd304a112c9a18d37a2d31ba9fb32bc5
Emil Forsén <emilxforsen@gmail.com>
2023-10-30 14:44:19 +0100

    Added constructor for startpos character

--- Beef System/CharacterEntity.h
+++ Beef System/CharacterEntity.h
@@ -9,20 +9,23 @@ class CharacterEntity :
 public:
 
 	std::string sprite = "assets/default.png";
-	float startX, startY;
+	float startX = 0;
+	float startY = 0;
 	float rotation;
 	std::string tag = "character";
 
 	CharacterEntity() = default;
 
-	CharacterEntity(const std::string& sprite)
+	CharacterEntity(const std::string& sprite, float x, float y)
 	{
 		this->sprite = sprite;
+		startX = x;
+		startY = y;
 	}
 
 	void init() override
 	{
-		this->addComponent<TransformComponent>();
+		this->addComponent<TransformComponent>(startX, startY);
 		this->addComponent<RotationComponent>();
 		this->addComponent<SpriteComponent>(sprite);
 		this->addComponent<Controller>();
