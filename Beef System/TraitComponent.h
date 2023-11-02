032e4afc4acfc075f7f6a871c1c8759c92f276c6
Emil Forsén <emilxforsen@gmail.com>
2023-11-02 08:47:50 +0100

    Added a basic traitcomponent

--- Beef System/TraitComponent.h
+++ Beef System/TraitComponent.h
@@ -1,7 +1,51 @@
 #pragma once
 #include "EntityComponentSystem.h"
+#include <string>
+#include <unordered_map>
+
 class TraitComponent :
     public Component
 {
+struct CharacterTraits
+{
+	std::unordered_map<std::string, float> traits;
+
+	CharacterTraits()
+	{
+		traits["Patience"] = 100.0f;
+		traits["Vocabulary"] = 50.0f;
+	}
+};
+
+
+public:
+
+	CharacterTraits characterTraits;
+
+	float getTrait(const std::string& traitName) const
+	{
+		auto trait = characterTraits.traits.find(traitName);
+		if (trait != characterTraits.traits.end())
+		{
+			return trait->second;
+		}
+
+		return 0.0f;
+	}
+
+	void setTrait(const std::string& traitName, float value)
+	{
+		characterTraits.traits[traitName] = value;
+	}
+
+	void modifyTrait(const std::string& traitName, float value)
+	{
+		auto trait = characterTraits.traits.find(traitName);
+		if (trait != characterTraits.traits.end())
+		{
+			characterTraits.traits[traitName] += value;
+		}
+	}
+
 };
 
